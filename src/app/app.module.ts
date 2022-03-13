6f05e34b49373345570f0cd0f511f4715f6149ea
Michaelle Lima <michaellelima22@gmail.com>
2022-03-13 16:12:52 -0300

    feat: importando novos modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,18 @@ import { NavComponent } from './components/template/nav/nav.component'
 import {MatSidenavModule} from '@angular/material/sidenav'
 import {MatListModule} from '@angular/material/list'
 import {MatCardModule} from '@angular/material/card'
+import {MatButtonModule} from '@angular/material/button'
 import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component'
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
 
 
 @NgModule({
@@ -38,7 +44,13 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatSidenavModule,
     MatListModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
